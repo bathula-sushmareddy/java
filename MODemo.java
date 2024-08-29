688e6c4bacbb8cd722ca4f8e084c39b5bd4eb4e9
bathula-sushmareddy <bathulasushma46@gmail.com>
2024-08-29 16:03:11 +0530

    Add files via upload

---
+++ MODemo.java
@@ -0,0 +1,41 @@
+class Employee {
+    public static int base = 10000;
+    int salary() {
+        return base;
+    }
+}
+ 
+// Inherited class
+class Manager extends Employee {
+    // This method overrides show() of Parent
+    int salary() {
+        return base + 20000;
+    }
+}
+ 
+// Inherited class
+class Clerk extends Employee {
+    // This method overrides show() of Parent
+    int salary(){
+        return base + 10000;
+    }
+}
+ 
+// Driver class
+class MODemo {
+    // This method can be used to print salary of any type of employee using base class refernce
+    static void printSalary(Employee e) {
+        System.out.println(e.salary());
+    }
+ 
+    public static void main(String[] args)
+    {
+        Manager obj1 = new Manager();
+        System.out.print("Manager's salary : ");
+        printSalary(obj1);
+ 
+        Employee obj2 = new Clerk();
+        System.out.print("Clerk's salary : ");
+        printSalary(obj2);
+    }
+}
